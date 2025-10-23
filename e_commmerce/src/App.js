48c7f10df6cc221bd1c5496383beaaa89e7c32d6
Nabi1245 <nabiansari8451@gmail.com>
2025-10-23 13:21:45 +0530

    project start amir sir

--- e_commmerce/src/App.js
+++ e_commmerce/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>you can worked start now this location</p>
+        <p>you can worked start now this location hi aamir sir</p>
       </header>
     </div>
   );
